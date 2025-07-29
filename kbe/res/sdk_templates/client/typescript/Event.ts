c6eb282781d176d01c7a37f9dab7a5b30a7bf0dd
IvanDing <ivan.ding95@qq.com>
2025-07-30 01:59:26 +0800

    添加内置的Event

--- kbe/res/sdk_templates/client/typescript/Event.ts
+++ kbe/res/sdk_templates/client/typescript/Event.ts
@@ -108,3 +108,214 @@ export default class KBEEvent
         KBEDebug.DEBUG_MSG("KBEEvent::DeregisterObject %s:delete count:%d.", p_object.toString(), deleteCount);
     }
 }
+
+
+// KBE-Plugin fire-out events(KBE => TS):
+export class EventOutTypes {
+    // ------------------------------------账号相关------------------------------------
+
+    
+    /** Create account feedback results. 
+    <para> param1(uint16): retcode. server_errors</para>
+    <para> param2(bytes): datas. If you use third-party account system, the system may fill some of the third-party additional datas. </para> */
+    static onCreateAccountResult = "onCreateAccountResult";
+
+    
+    /** Response from binding account Email request.
+    <para> param1(uint16): retcode. server_errors</para> */
+    static onBindAccountEmail = "onBindAccountEmail";
+
+    
+    /** Response from a new password request.
+    <para> param1(uint16): retcode. server_errors</para> */
+    static onNewPassword = "onNewPassword";
+
+    
+    /** Response from a reset password request.
+    <para> param1(uint16): retcode. server_errors</para> */
+    static onResetPassword = "onResetPassword";
+
+    // ------------------------------------连接相关------------------------------------
+    
+    /** Kicked of the current server.
+    <para> param1(uint16): retcode. server_errors</para> */
+    static onKicked = "onKicked";
+
+    
+    /** Disconnected from the server. */
+    static onDisconnected = "onDisconnected";
+
+    
+    /** Status of connection server.
+    <para> param1(bool): success or fail</para> */
+    static onConnectionState = "onConnectionState";
+
+    // ------------------------------------logon相关------------------------------------
+    
+    /** Engine version mismatch.
+    <para> param1(string): clientVersion
+    <para> param2(string): serverVersion */
+    static onVersionNotMatch = "onVersionNotMatch";
+
+    
+    /** script version mismatch.
+    <para> param1(string): clientScriptVersion
+    <para> param2(string): serverScriptVersion */
+    static onScriptVersionNotMatch = "onScriptVersionNotMatch";
+
+    
+    /** Login failed.
+    <para> param1(uint16): retcode. server_errors</para> */
+    static onLoginFailed = "onLoginFailed";
+
+    
+    /** Login to baseapp. */
+    static onLoginBaseapp = "onLoginBaseapp";
+
+    
+    /** Login baseapp failed.
+    <para> param1(uint16): retcode. server_errors</para> */
+    static onLoginBaseappFailed = "onLoginBaseappFailed";
+
+    
+    /** Relogin to baseapp. */
+    static onReloginBaseapp = "onReloginBaseapp";
+
+    
+    /** Relogin baseapp success. */
+    static onReloginBaseappSuccessfully = "onReloginBaseappSuccessfully";
+
+    
+    /** Relogin baseapp failed.
+    <para> param1(uint16): retcode. server_errors</para> */
+    static onReloginBaseappFailed = "onReloginBaseappFailed";
+
+    //------------------------------------实体cell相关事件------------------------------------
+
+    
+    /** Entity enter the client-world.
+    <para> param1: Entity</para> */
+    static onEnterWorld = "onEnterWorld";
+
+    
+    /** Entity leave the client-world.
+    <para> param1: Entity</para> */
+    static onLeaveWorld = "onLeaveWorld";
+
+    
+    /** Player enter the new space.
+    <para> param1: Entity</para> */
+    static onEnterSpace = "onEnterSpace";
+
+    
+    /** Player leave the space.
+    <para> param1: Entity</para> */
+    static onLeaveSpace = "onLeaveSpace";
+
+    
+    /** Sets the current position of the entity.
+    <para> param1: Entity</para> */
+    static set_position = "set_position";
+
+    
+    /** Sets the current direction of the entity.
+    <para> param1: Entity</para> */
+    static set_direction = "set_direction";
+
+    
+    /** The entity position is updated, you can smooth the moving entity to new location.
+    <para> param1: Entity</para> */
+    static updatePosition = "updatePosition";
+
+    
+    /** The current space is specified by the geometry mapping.
+    Popular said is to load the specified Map Resources.
+    <para> param1(string): resPath</para> */
+    static addSpaceGeometryMapping = "addSpaceGeometryMapping";
+
+    
+    /** Server spaceData set data.
+    <para> param1(int32): spaceID</para>
+    <para> param2(string): key</para>
+    <para> param3(string): value</para> */
+    static onSetSpaceData = "onSetSpaceData";
+
+    
+    /** Start downloading data.
+    <para> param1(int32): rspaceID</para>
+    <para> param2(string): key</para> */
+    static onDelSpaceData = "onDelSpaceData";
+
+    
+    /** Triggered when the entity is controlled or out of control.
+    <para> param1: Entity</para>
+    <para> param2(bool): isControlled</para> */
+    static onControlled = "onControlled";
+
+    
+    /** Lose controlled entity.
+    <para> param1: Entity</para> */
+    static onLoseControlledEntity = "onLoseControlledEntity";
+
+    // ------------------------------------数据下载相关------------------------------------
+    
+    /** Start downloading data.
+    <para> param1(uint16): resouce id</para>
+    <para> param2(uint32): data size</para>
+    <para> param3(string): description</para> */
+    static onStreamDataStarted = "onStreamDataStarted";
+
+    
+    /** Receive data.
+    <para> param1(uint16): resouce id</para>
+    <para> param2(bytes): datas</para> */
+    static onStreamDataRecv = "onStreamDataRecv";
+
+    
+    /** The downloaded data is completed.
+    <para> param1(uint16): resouce id</para> */
+    static onStreamDataCompleted = "onStreamDataCompleted";
+};
+
+
+//KBE-Plugin fire-in events(TS => KBE):
+
+export class EventInTypes {
+    
+    /** Create new account.
+    <para> param1(string): accountName</para>
+    <para> param2(string): password</para>
+    <para> param3(bytes): datas Datas by user defined. Data will be recorded into the KBE account database, you can access the datas through the script layer. If you use third-party account system, datas will be submitted to the third-party system.</para> */
+    static createAccount = "createAccount";
+
+    
+    /** Login to server.
+    <para> param1(string): accountName</para>
+    <para> param2(string): password</para>
+    <para> param3(bytes): datas Datas by user defined. Data will be recorded into the KBE account database, you can access the datas through the script layer. If you use third-party account system, datas will be submitted to the third-party system.</para> */
+    static login = "login";
+
+    
+    /** Logout to baseapp, called when exiting the client. */
+    static logout = "logout";
+
+    
+    /** Relogin to baseapp. */
+    static reloginBaseapp = "reloginBaseapp";
+
+    
+    /** Reset password.
+    <para> param1(string): accountName</para> */
+    static resetPassword = "resetPassword";
+
+    
+    /** Request to set up a new password for the account. Note: account must be online.
+    <para> param1(string): old_password</para>
+    <para> param2(string): new_password</para> */
+    static newPassword = "newPassword";
+
+    
+    /** Request server binding account Email.
+    <para> param1(string): emailAddress</para> */
+    static bindAccountEmail = "bindAccountEmail";
+};
