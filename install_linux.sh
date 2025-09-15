95b8272a38e4d8219d80b9a0751e0029e2416d32
IvanDing <ivan.ding95@qq.com>
2025-09-16 05:10:48 +0800

    兼容centos8和10

--- install_linux.sh
+++ install_linux.sh
@@ -53,7 +53,8 @@ update_pkg_index
 
 install_dep() {
     NAME="$1"
-    shift
+    IGNORE_FAIL="$2"
+    shift 2
     for PKG in "$@"; do
         # 检查是否已安装
         if $PKG_CHECK "$PKG" >/dev/null 2>&1; then
@@ -70,7 +71,18 @@ install_dep() {
         fi
     done
     echo "[WARN] 未能安装 $NAME，请手动安装 (候选: $*)"
-    return 1
+    if [ "$IGNORE_FAIL" = true ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+
+# 检测是否为 CentOS / RHEL / Fedora
+is_centos() {
+    [[ -f /etc/os-release ]] && grep -qiE 'centos|rhel|fedora' /etc/os-release
 }
 
 # =========================================
@@ -82,47 +94,67 @@ echo "[INFO] 使用构建类型: $KBE_CONFIG"
 # =========================================
 # 基础工具
 # =========================================
-install_dep "Git" git
-install_dep "GCC" gcc
-install_dep "G++" g++ gcc-c++
-install_dep "Make" make
-install_dep "Autoconf" autoconf
-install_dep "Libtool" libtool-bin libtool
-install_dep "CMake" cmake
-install_dep "pkg-config" pkg-config pkgconf-pkg-config pkgconf
-install_dep "Build Tools" build-essential "@development-tools" base-devel base-devel "Development Tools"
-install_dep "curl" curl
-install_dep "zip" zip
-install_dep "unzip" unzip
-install_dep "tar" tar
+install_dep "Git" false git
+install_dep "GCC" false gcc
+install_dep "G++" false g++ gcc-c++
+install_dep "Make" false make
+install_dep "Autoconf" false autoconf
+install_dep "Libtool" false libtool-bin libtool
+install_dep "CMake" false cmake
+install_dep "pkg-config" false pkg-config pkgconf-pkg-config pkgconf
 
-# 检测是否是基于 RedHat 的系统
-if [ -f /etc/redhat-release ] || grep -qiE "centos|fedora|red hat" /etc/os-release; then
-    install_dep "perl-IPC-Cmd" perl-IPC-Cmd
+install_dep "curl" false curl
+install_dep "zip" false zip
+install_dep "unzip" false unzip
+install_dep "tar" false tar
+
+# 安装 Build Tools，只在非 CentOS 系列执行
+if ! is_centos; then
+    install_dep "Build Tools"  false build-essential "@development-tools" base-devel
 else
-    echo "[INFO] 非 CentOS/RHEL/Fedora 系统，跳过 perl-IPC-Cmd 安装"
+    echo "[INFO] Skipping 'Build Tools' installation on CentOS/RHEL/Fedora"
 fi
+
 
 
 # =========================================
 # 额外依赖
 # =========================================
-install_dep "TIRPC" libtirpc-dev libtirpc-devel
-install_dep "MySQL/MariaDB" \
+
+if is_centos; then
+    # 安装 dnf-plugins-core
+    install_dep "dnf-plugins-core" true dnf-plugins-core 
+    sudo dnf config-manager --set-enabled crb  || true
+else
+    echo "[INFO] 非 CentOS/RHEL/Fedora 系统，跳过 dnf-plugins-core 安装"
+fi
+
+
+install_dep "TIRPC"  false libtirpc-dev libtirpc-devel
+install_dep "MySQL/MariaDB"  false \
     libmysqlclient-dev libmariadb-dev \
     mariadb-devel mysql-devel mariadb-connector-c-devel \
     libmariadb-devel libmysqlclient-devel \
     mariadb-clients mariadb-libs \
     mariadb-connector-c-dev mysql-dev
-install_dep "libffi" libffi-dev libffi-devel libffi
-install_dep "UUID" uuid-dev libuuid-devel util-linux-dev
-install_dep "BZip2" libbz2-dev bzip2-devel libbz2-devel bzip2
-install_dep "OpenSSL" libssl-dev openssl-devel openssl-dev
-install_dep "Zlib" zlib1g-dev zlib-devel zlib-dev
-install_dep "CURL Dev" libcurl4-openssl-dev libcurl-devel curl-dev
+install_dep "libffi"  false libffi-dev libffi-devel libffi
+install_dep "UUID"  false uuid-dev libuuid-devel util-linux-dev
+install_dep "BZip2"  false libbz2-dev bzip2-devel libbz2-devel bzip2
+install_dep "OpenSSL"  false libssl-dev openssl-devel openssl-dev
+install_dep "Zlib"  false zlib1g-dev zlib-devel zlib-dev
+install_dep "CURL Dev"  false libcurl4-openssl-dev libcurl-devel curl-dev
 
 
 
+if is_centos; then
+    # 安装 Perl
+    install_dep "perl" false perl
+    install_dep "perl-core" false perl-core
+    install_dep "perl-IPC-Cmd" false perl-IPC-Cmd
+    install_dep "perl-FindBin" true perl-FindBin
+else
+    echo "[INFO] 非 CentOS/RHEL/Fedora 系统，跳过 perl安装"
+fi
 
 # =========================================
 # GitHub 可访问性检查
