43f13ee30fc0b464a96f2d8d49ceb79aed8480fd
IvanDing <ivan.ding95@qq.com>
2025-11-11 00:11:54 +0800

    修复 https://github.com/KBEngineLab/KBEngine-Nex/issues/53

--- kbe/tools/server/webconsole/webconsole/signals.py
+++ kbe/tools/server/webconsole/webconsole/signals.py
@@ -3,11 +3,11 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import KBEUserExtension
 
-@receiver(post_save, sender=User)
-def create_kbe_user_extension(sender, instance, created, **kwargs):
-    if created:
-        KBEUserExtension.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_kbe_user_extension(sender, instance, created, **kwargs):
+#     if created:
+#         KBEUserExtension.objects.get_or_create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_kbe_user_extension(sender, instance, **kwargs):
-    instance.kbeuserextension.save()
+# @receiver(post_save, sender=User)
+# def save_kbe_user_extension(sender, instance, **kwargs):
+#     instance.kbeuserextension.save()
